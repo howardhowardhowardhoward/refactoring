3c9502ba71c736343e0ac9a81259ad6a5b86a501
howardzhang <zhanghoward2016@gmail.com>
2025-11-20 17:09:28 -0500

    style fixes

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,27 @@
+
 package theater;
 
+/**
+ * Represents a play in the theater system.
+ *
+ * @author Howard Zhang
+ * @version 1.0
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
